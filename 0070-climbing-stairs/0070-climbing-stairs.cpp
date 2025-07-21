94290b55b1fd0132f8ded77305a3cd83d0229438
armaan-monga <mearmaan661@gmaIl.com>
2025-07-21 19:18:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,14 +5,13 @@ public:
 
     int one_step_before = 1;   
     int two_steps_before = 1;  
-    int current_ways = 0;
 
     for (int i = 2; i <= n;i++) {
-        current_ways = one_step_before + two_steps_before;
+        int current_ways = one_step_before + two_steps_before;
         two_steps_before = one_step_before;
         one_step_before = current_ways;
     }
 
-    return current_ways;
+    return one_step_before;
     }
 };
