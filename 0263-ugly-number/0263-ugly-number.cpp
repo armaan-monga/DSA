d726465e74fb4fb602dfce9d40f2a61b7f4430f2
armaan-monga <mearmaan661@gmaIl.com>
2025-07-25 18:43:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (78.4%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+         if (n <= 0) return false;
+        
+        int divisors[] = {2, 3, 5};
+        
+        for (int d : divisors) {
+            while (n % d == 0) {
+                n /= d;
+            }
+        }
+        
+        return n == 1;
+    }
+};
