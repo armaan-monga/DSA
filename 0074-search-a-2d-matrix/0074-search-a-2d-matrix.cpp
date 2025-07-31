594e4cfe734479fc49f90ae8e2ce75f3eafeba00
armaan-monga <mearmaan661@gmaIl.com>
2025-07-31 12:55:12 +0530

    Time: 0 ms (100%), Space: 13.2 MB (75.96%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // int n=matrix.size();
+        // int m=matrix[0].size();
+        // for(int i=0;i<n;i++){
+        //     if(matrix[i][0]<=target && target<=matrix[i][m-1]){
+        //     int st=0,end=m-1;
+        //     while(st<=end){
+        //         int mid=st+(end-st)/2;
+        //         if(matrix[i][mid]==target) return 1;
+        //         else if(matrix[i][mid]<target){
+        //             st=mid+1;
+        //         }
+        //         else{
+        //             end=mid-1;
+        //         }
+        //     }
+        //     }
+        // }return 0;
+
         int n=matrix.size();
         int m=matrix[0].size();
-        for(int i=0;i<n;i++){
-            if(matrix[i][0]<=target && target<=matrix[i][m-1]){
-            int st=0,end=m-1;
-            while(st<=end){
-                int mid=st+(end-st)/2;
-                if(matrix[i][mid]==target) return 1;
-                else if(matrix[i][mid]<target){
-                    st=mid+1;
-                }
-                else{
-                    end=mid-1;
-                }
-            }
-            }
-        }return 0;
+        int st=0,end=m*n-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            int row_index=mid/m;
+            int col_index=mid%m;
+            if(matrix[row_index][col_index]==target) return 1;
+            else if(matrix[row_index][col_index]<target) st=mid+1;
+            else end=mid-1;
+
+        }
+            return 0;
+
     }
 };
