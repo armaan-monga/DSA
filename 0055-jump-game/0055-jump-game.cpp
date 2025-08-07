f7cec16d1d143347866c029d73e5731e8d70ec4a
armaan-monga <mearmaan661@gmaIl.com>
2025-08-07 18:10:52 +0530

    Time: 0 ms (100%), Space: 52.3 MB (33.54%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-         int maxReach = 0; // furthest index we can reach
+        int maxReach = 0;
         int n = nums.size();
 
         for (int i = 0; i < n; ++i) {
             if (i > maxReach) {
-                // can't reach this position
                 return false;
             }
             maxReach = max(maxReach, i + nums[i]);
             if (maxReach >= n - 1) {
-                return true; // can reach or pass the last index
-            }
+                return true;  
+           }
         }
         return false;
     }
