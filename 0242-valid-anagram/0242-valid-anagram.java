c96e0a1ae8fd146474fb840aa7cc0ba147dd88fa
armaan-monga <mearmaan661@gmaIl.com>
2025-10-16 16:52:57 +0530

    Time: 15 ms (23.9%), Space: 44.6 MB (43.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,10 +1,23 @@
 class Solution {
+    static HashMap<Character,Integer> makefreqmap(String str){
+        HashMap<Character,Integer> mp=new HashMap<>();
+        for(int i=0;i<str.length();i++){
+            Character ch=str.charAt(i);
+            if(!mp.containsKey(ch)){
+                mp.put(ch,1);
+            }
+            else{
+                int currfreq=mp.get(ch);
+                mp.put(ch,currfreq+1);
+            }
+        }
+        return mp;
+    }
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        char[] sarr=s.toCharArray();
-        char[] tarr=t.toCharArray();
-        Arrays.sort(sarr);
-        Arrays.sort(tarr);
-        return Arrays.equals(sarr,tarr);
+        HashMap<Character,Integer> mp1=makefreqmap(s);
+        HashMap<Character,Integer> mp2=makefreqmap(t);
+        return mp1.equals(mp2);
+        
     }
 }
