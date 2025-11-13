7b8834a07852d87c0f760b1f3a20e91d7145017f
armaan-monga <mearmaan661@gmaIl.com>
2025-11-13 19:28:29 +0530

    Time: 0 ms (100%), Space: 45.2 MB (16.07%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int height(TreeNode root){
+        if(root==null)return 0;
+        return Math.max(height(root.left),height(root.right))+1;
+    }
+    public boolean isBalanced(TreeNode root) {
+        if(root ==null) return true;
+        int left=height(root.left);
+        int right=height(root.right);
+        if(Math.abs(left-right)>1)return false;
+        return isBalanced(root.left) && isBalanced(root.right);    }
+}
