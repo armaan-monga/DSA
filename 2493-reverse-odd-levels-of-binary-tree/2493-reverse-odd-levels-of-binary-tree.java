3d597b50ae0bf84a42ba649c70586e01024a3f7c
armaan-monga <mearmaan661@gmaIl.com>
2025-11-19 17:55:25 +0530

    Time: 0 ms (100%), Space: 49.4 MB (31%) - LeetHub

---
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void odd(TreeNode left,TreeNode right,int level){
+        if(left==null||right==null)return;
+        if(level%2==1){
+            int temp=left.val;
+            left.val=right.val;
+            right.val=temp;
+        }
+        odd(left.left,right.right,level+1);
+        odd(left.right,right.left,level+1);
+    }
+    public TreeNode reverseOddLevels(TreeNode root) {
+        if(root==null)return null;
+        odd(root.left,root.right,1);
+        return root;
+    }
+}
