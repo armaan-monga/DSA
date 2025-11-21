80476c55269902c4f18bc51df657a10935682b37
armaan-monga <mearmaan661@gmaIl.com>
2025-11-21 20:23:54 +0530

    Time: 1 ms (99.87%), Space: 47.1 MB (99.66%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -1,46 +1,33 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode tempA=headA;
-        ListNode tempB=headB;
-        int lengthA=0;
-        while(tempA!=null){
-            lengthA++;
-            tempA=tempA.next;
+        ListNode temp1=headA;
+        ListNode temp2=headB;
+        int len1=0;
+        int len2=0;
+        while(temp1!=null){
+            temp1=temp1.next;
+            len1++;
         }
-        int lengthB=0;
-        while(tempB!=null){
-            lengthB++;
-            tempB=tempB.next;
+        while(temp2!=null){
+            temp2=temp2.next;
+            len2++;
         }
-        tempA=headA;
-        tempB=headB;
-        if(lengthA>lengthB){
-            int steps=lengthA-lengthB;
-            for(int i=1;i<=steps;i++){
-                tempA=tempA.next;
+        temp1=headA;
+        temp2=headB;
+        if(len1>len2){
+            for(int i=0;i<len1-len2;i++){
+                temp1=temp1.next;
             }
         }
-            else{
-                int steps=lengthB-lengthA;
-                for(int i=1;i<=steps;i++){
-                    tempB=tempB.next;
-                }
+        else{
+            for(int i=0;i<len2-len1;i++){
+                temp2=temp2.next;
             }
-        while(tempA!=tempB){
-            tempA=tempA.next;
-            tempB=tempB.next;
         }
-        return tempA;
+        while(temp1!=temp2){
+            temp1=temp1.next;
+            temp2=temp2.next;
+        }
+        return temp1;
     }
 }
