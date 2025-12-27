f685f490e767dd9be6478569e2896c9253b7ba12
armaan-monga <mearmaan661@gmaIl.com>
2025-12-27 13:48:10 +0530

    Time: 1 ms (99.91%), Space: 52.9 MB (17.31%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -1,33 +1,38 @@
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode temp1=headA;
-        ListNode temp2=headB;
-        int len1=0;
-        int len2=0;
-        while(temp1!=null){
-            temp1=temp1.next;
-            len1++;
+    public ListNode getIntersectionNode(ListNode heada, ListNode headb) {
+        if(heada==null && headb==null)return null;
+        if(heada==null || headb==null)return null;
+        ListNode tempa=heada;
+        ListNode tempb=headb;
+        int sizea=0;
+        int sizeb=0;
+        while(tempa!=null){
+            sizea++;
+            tempa=tempa.next;
         }
-        while(temp2!=null){
-            temp2=temp2.next;
-            len2++;
+        while(tempb!=null){
+            sizeb++;
+            tempb=tempb.next;
         }
-        temp1=headA;
-        temp2=headB;
-        if(len1>len2){
-            for(int i=0;i<len1-len2;i++){
-                temp1=temp1.next;
+        tempa=heada;
+        tempb=headb;
+        if(sizea>sizeb){
+            while(sizea-sizeb>0){
+                tempa=tempa.next;
+                sizea--;
             }
         }
         else{
-            for(int i=0;i<len2-len1;i++){
-                temp2=temp2.next;
+            while(sizeb-sizea>0){
+                tempb=tempb.next;
+                sizeb--;
             }
         }
-        while(temp1!=temp2){
-            temp1=temp1.next;
-            temp2=temp2.next;
+        while(tempa!=null && tempb!=null){
+            if(tempa==tempb)return tempb;
+            tempa=tempa.next;
+            tempb=tempb.next;
         }
-        return temp1;
+        return null;
     }
 }
