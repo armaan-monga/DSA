42e21a47153189f67f88a8f1d7ba769164f41e01
armaan-monga <mearmaan661@gmaIl.com>
2025-12-31 14:29:12 +0530

    Time: 1 ms (99.79%), Space: 77.1 MB (80.57%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,10 @@
 class Solution {
-    public int maxSubArray(int[] nums) {
-        HashMap<Integer,Integer> mp=new HashMap<>();
-        int n=nums.length;
-        int maxsum=Integer.MIN_VALUE;
+    public int maxSubArray(int[] arr) {
         int currsum=0;
-        for(int i=0;i<n;i++){
-            currsum+=nums[i];
+        int maxsum=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            currsum=Math.max(arr[i],currsum+arr[i]);
             maxsum=Math.max(currsum,maxsum);
-            if(currsum<0)currsum=0;
         }
         return maxsum;
     }
