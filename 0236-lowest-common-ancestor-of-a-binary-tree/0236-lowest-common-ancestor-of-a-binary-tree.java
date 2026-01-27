0ac99cc898245b0a23993df609f3cc5b3164940b
armaan-monga <mearmaan661@gmaIl.com>
2026-01-27 22:01:04 +0530

    Time: 438 ms (5.02%), Space: 46.3 MB (94.12%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,17 +1,17 @@
 class Solution {
-    public static boolean contains(TreeNode root,TreeNode node){
+    public static boolean contains(TreeNode root,TreeNode x){
         if(root==null)return false;
-        if(root==node)return true;
-        return contains(root.left,node)||contains(root.right,node);
+        if(root==x)return true;
+        return contains(root.left,x) || contains(root.right,x);
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(p==root || q==root)return root;
-        if(p==q)return p;
-        boolean leftp=contains(root.left,p);
+        boolean  leftp=contains(root.left,p);
         boolean rightq=contains(root.right,q);
-        if((leftp && rightq) || (!leftp && !rightq))return root;
-        if(leftp==true && rightq==false)return lowestCommonAncestor(root.left,p,q);
-        if(leftp==false && rightq==true)return lowestCommonAncestor(root.right,p,q);
+        if(leftp && rightq)return root;
+        if(!leftp && !rightq)return root;
+        if(!leftp && rightq)return lowestCommonAncestor(root.right,p,q);
+        if(leftp && !rightq)return lowestCommonAncestor(root.left,p,q);
         return root;
     }
 }
