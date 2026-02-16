f00afe1ce9c7383d313f39e6bbf686921c63d70b
armaan-monga <mearmaan661@gmaIl.com>
2026-02-16 14:56:01 +0530

    Time: 0 ms (100%), Space: 47.9 MB (15.79%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void delete(TreeNode root,int target){
+        if(root==null)return;
+        if(root.val>target){
+            if(root.left==null)return;
+            if(root.left.val==target){
+            TreeNode l=root.left;
+            if(l.left==null && l.right==null)root.left=null;
+            else if(l.left==null || l.right==null){
+                if(l.left!=null)root.left=l.left;
+                else root.left=l.right;
+            }
+            else{
+                TreeNode curr=l;
+                TreeNode pred=curr.left;
+                while(pred.right!=null)pred=pred.right;
+                delete(root,pred.val);
+                pred.left=curr.left;
+                pred.right=curr.right;
+                root.left=pred;
+            }
+        }
+        else delete(root.left,target);
+        }
+        
+        if(root.val<target){
+            if(root.right==null)return;
+            if(root.right.val==target){
+            TreeNode r=root.right;
+            if(r.left==null && r.right==null)root.right=null;
+            else if(r.left==null || r.right==null){
+                if(r.left!=null)root.right=r.left;
+                else root.right=r.right;
+            }
+            else{
+                TreeNode curr=r;
+                TreeNode pred=curr.left;
+                while(pred.right!=null)pred=pred.right;
+                delete(root,pred.val);
+                pred.left=curr.left;
+                pred.right=curr.right;
+                root.right=pred;
+            }
+        }
+        else delete(root.right,target);
+        }
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+       TreeNode temp=new TreeNode(Integer.MAX_VALUE);
+       temp.left=root;
+       delete(temp,key);
+       return temp.left; 
+    }
+}
