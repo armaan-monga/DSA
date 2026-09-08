fe91dcaf475e458fa4dfbf30de281b32900ef68b
armaan-monga <mearmaan661@gmaIl.com>
2026-09-08 10:19:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (37.80%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,15 +1,15 @@
 class Solution {
     int[] dp;
     public int solve(int i,int[] nums){
-        if(i>=nums.length)return 0;
+        if(i<0)return 0;
         if(dp[i]!=-1)return dp[i];
-        int take=nums[i]+solve(i+2,nums);
-        int skip=solve(i+1,nums);
+        int take=nums[i]+solve(i-2,nums);
+        int skip=solve(i-1,nums);
         return dp[i]=Math.max(take,skip);
     }
     public int rob(int[] nums) {
         dp=new int[nums.length];
         Arrays.fill(dp,-1);
-        return solve(0,nums);
+        return solve(nums.length-1,nums);
     }
 }
