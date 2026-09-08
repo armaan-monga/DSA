006c3451d1ab4b7caff70c76874cf4e86908185b
armaan-monga <mearmaan661@gmaIl.com>
2026-09-08 08:52:52 +0530

    Add solution for Count Commas in Range

--- LeetCode/Count-Commas-in-Range/Solution.java
+++ LeetCode/Count-Commas-in-Range/Solution.java
@@ -1,28 +1,25 @@
 1class Solution {
 2    public int countCommas(int n) {
 3        if(n<1000)return 0;
-4        int len=0;
-5        int num=n;
-6        while(num>0){
-7            len++;
-8            num/=10;
-9        }
-10        int ans=1;
-11        if(len==4){
-12          ans=n-1000;  
-13        }
-14        if(len==5){
-15          ans=(9000)+n-10000;  
-16        }
-17        if(len==6){
-18          ans=(99000)+n-100000;  
-19        }
-20        // if(len==4){
-21        //   ans=n-1000;  
-22        // }
-23        // if(len==4){
-24        //   ans=n-1000;  
-25        // }
-26        return ans+1;
-27    }
-28}
+4        // int len=0;
+5        // int num=n;
+6        // while(num>0){
+7        //     len++;
+8        //     num/=10;
+9        // }
+10        // int ans=1;
+11        // if(len==4){
+12        //   ans=n-1000;  
+13        // }
+14        // if(len==5){
+15        //   ans=(9000)+n-10000;  
+16        // }
+17        // if(len==6){
+18        //   ans=(99000)+n-100000;  
+19        // }
+20        // return ans+1;
+21        return n-999;
+22    }
+23}
+24
+25
