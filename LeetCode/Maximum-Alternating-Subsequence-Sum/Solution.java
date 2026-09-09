8068c246809a58cf465fdbb55fa8fc3d04676e77
armaan-monga <mearmaan661@gmaIl.com>
2026-09-09 20:57:13 +0530

    Add solution for Maximum Alternating Subsequence Sum

--- LeetCode/Maximum-Alternating-Subsequence-Sum/Solution.java
+++ LeetCode/Maximum-Alternating-Subsequence-Sum/Solution.java
@@ -1,20 +1,17 @@
 1class Solution {
-2    long[][] dp;
-3    public long helper(int i,int[] nums,boolean flag){
-4        if(i<0)return 0;
-5        int state=flag?1:0;
-6        if(dp[i][state]!=-1)return dp[i][state];
-7        long skip=helper(i-1,nums,flag);
-8        int val=nums[i];
-9        if(flag==false)val=-val;
-10        long pick=val+helper(i-1,nums,!flag);
-11        return dp[i][state]=Math.max(skip,pick);
-12    }
-13    public long maxAlternatingSum(int[] nums) {
-14        dp=new long[nums.length][2];
-15        for(long[] v:dp){
-16            Arrays.fill(v,-1);
-17        }
-18        return helper(nums.length-1,nums,true);
-19    }
-20}
+2    public long maxAlternatingSum(int[] nums) {
+3        int n = nums.length;
+4        long[][] dp = new long[n + 1][2];
+5        for (int i = n - 1; i >= 0; i--) {
+6            dp[i][1] = Math.max(
+7                nums[i] + dp[i + 1][0],
+8                dp[i + 1][1]
+9            );
+10            dp[i][0] = Math.max(
+11                -nums[i] + dp[i + 1][1],
+12                dp[i + 1][0]
+13            );
+14        }
+15        return dp[0][1];
+16    }
+17}
