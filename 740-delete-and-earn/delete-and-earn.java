5b996c09f9c3f8253a6338a40ba543d0eb6ee3ac
armaan-monga <mearmaan661@gmaIl.com>
2026-09-10 00:27:04 +0530

    Time: 3 ms (73.02%) | Memory: 46.4 MB (89.47%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,35 +1,31 @@
 class Solution {
-    int[] dp;
-    HashMap<Integer, Integer> map;
-    public int solve(int i) {
-        if (i <= 0) {
-            return 0;
-        }
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        int skip = solve(i - 1);
-        int take = map.getOrDefault(i, 0) + solve(i - 2);
-        return dp[i] = Math.max(skip, take);
-    }
+
     public int deleteAndEarn(int[] nums) {
-        map = new HashMap<>();
+
         int max = 0;
+
+        int[] points = new int[10001];
+
+        // Calculate total points for each number
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + num);
+            points[num] += num;
             max = Math.max(max, num);
         }
-        dp = new int[max + 1];
-        int a =0;
-        int b=0;
-        int c=0;
-        if (max >= 1)
-        b = map.getOrDefault(1, 0);
-        for(int i=2;i<=max;i++){
-            c=Math.max(b,map.getOrDefault(i,0)+a);
-            a=b;
-            b=c;
+
+        int a = 0; // dp[i-2]
+        int b = 0; // dp[i-1]
+
+        for (int i = 1; i <= max; i++) {
+
+            int c = Math.max(
+                b,              // skip i
+                points[i] + a   // take i
+            );
+
+            a = b;
+            b = c;
         }
+
         return b;
     }
 }
