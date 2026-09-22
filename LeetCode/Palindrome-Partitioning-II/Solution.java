1ff80f41b0ac5c2b2bad47d6bb888704b231037c
armaan-monga <mearmaan661@gmaIl.com>
2026-09-23 04:50:58 +0530

    Add solution for Palindrome Partitioning II

--- LeetCode/Palindrome-Partitioning-II/Solution.java
+++ LeetCode/Palindrome-Partitioning-II/Solution.java
@@ -1,31 +1,26 @@
 1class Solution {
-2    int ans=Integer.MAX_VALUE;
-3    int[] dp;
-4    public int minCut(String s) {
-5        dp=new int[s.length()];
-6        Arrays.fill(dp,-1);
-7        return solve(0,s)-1;
-8    }
-9    public boolean isPalindrome(String s,int left,int right){
-10        while(left<right){
-11            if(s.charAt(left++)!=s.charAt(right--))return false;
-12        }
-13        return true;
-14    }
-15    public int solve(int index,String s){
-16        if(index==s.length()){
-17            return 0;
-18        }
-19        if(dp[index]!=-1){
-20            return dp[index];
-21        }
-22        int min=Integer.MAX_VALUE;
-23        for(int i=index;i<s.length();i++){
-24            if(isPalindrome(s,index,i)){
-25                int partition=1+solve(i+1,s);
-26                min=Math.min(min,partition);
-27            }
-28        }
-29        return dp[index]=min;
-30    }
-31}
+2    public int minCut(String s) {
+3        int n = s.length();
+4        int[] dp = new int[n + 1];
+5        dp[n] = 0;
+6        for (int index = n - 1; index >= 0; index--) {
+7            int min = Integer.MAX_VALUE;
+8            for (int i = index; i < n; i++) {
+9                if (isPalindrome(s, index, i)) {
+10                    int partition = 1 + dp[i + 1];
+11                    min = Math.min(min, partition);
+12                }
+13            }
+14            dp[index] = min;
+15        }
+16        return dp[0] - 1;
+17    }
+18    public boolean isPalindrome(String s, int left, int right) {
+19        while (left < right) {
+20            if (s.charAt(left++) != s.charAt(right--)) {
+21                return false;
+22            }
+23        }
+24        return true;
+25    }
+26}
