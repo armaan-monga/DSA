685df508cbed99d54371d92bb1b3553dff9d35ea
armaan-monga <mearmaan661@gmaIl.com>
2026-09-22 17:14:10 +0530

    Time: 2 ms (56.54%) | Memory: 47.1 MB (83.56%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -1,24 +1,27 @@
 class Solution {
     public List<String> letterCasePermutation(String s) {
-        List<String> ans=new ArrayList<>();
-       solve(0,s,"",ans); 
-       return ans;
+        List<String> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        solve(0, s, sb, ans);
+        return ans;
     }
-    public void solve(int i,String s,String temp,List<String> ans){
-        if(i==s.length()){
-            ans.add(temp);
+    public void solve(int i, String s, StringBuilder sb, List<String> ans) {
+        if (i == s.length()) {
+            ans.add(sb.toString());
             return;
         }
         char ch = s.charAt(i);
-        if(Character.isLetter(ch)){
-            char lower=Character.toLowerCase(ch);
-            char upper = Character.toUpperCase(ch);
-            solve(i+1,s,temp+lower,ans);
-            solve(i+1,s,temp+upper,ans);
+        if (Character.isLetter(ch)) {
+            sb.append(Character.toLowerCase(ch));
+            solve(i + 1, s, sb, ans);
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append(Character.toUpperCase(ch));
+            solve(i + 1, s, sb, ans);
+            sb.deleteCharAt(sb.length() - 1);
+        } else {
+            sb.append(ch);
+            solve(i + 1, s, sb, ans);
+            sb.deleteCharAt(sb.length() - 1);
         }
-        else{
-            solve(i+1,s,temp+ch,ans);
-        }
-        return;
     }
 }
