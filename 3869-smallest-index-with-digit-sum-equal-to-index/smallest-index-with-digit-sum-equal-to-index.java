dee517c1e76158e04dc0c483eafc309c4b011fb9
armaan-monga <mearmaan661@gmaIl.com>
2026-09-24 17:43:23 +0530

    Time: 1 ms (99.69%) | Memory: 45.3 MB (89.71%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -1,6 +1,5 @@
 class Solution {
     public int smallestIndex(int[] nums) {
-       int ans = -1;
        for(int i=0;i<nums.length;i++){
         int digit = nums[i];
         int sum = 0;
@@ -11,6 +10,6 @@ class Solution {
         }
         if(sum == i)return i;
        }
-       return ans;
+       return -1;
     }
 }
